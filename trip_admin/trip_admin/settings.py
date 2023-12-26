7ffb5bf54fbe4a45f73d2eb170856aafb9b87c7a
rauf.akchurin <abdurauf.dev@gmail.com>
2023-12-26 12:10:50 +0500

    deploy V1

--- trip_admin/trip_admin/settings.py
+++ trip_admin/trip_admin/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dunatrip.ru', '5.35.88.35', '127.0.0.1']
 
 
 # Application definition
