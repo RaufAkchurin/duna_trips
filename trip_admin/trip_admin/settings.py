91138a804f70476e28a59b42c55a951b04eb7ee2
rauf.akchurin <abdurauf.dev@gmail.com>
2023-12-27 10:34:18 +0500

    deploy V2

--- trip_admin/trip_admin/settings.py
+++ trip_admin/trip_admin/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/root/duna_trip/trip_admin/static/'
+STATIC_ROOT = '/root/duna_trips/trip_admin/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
