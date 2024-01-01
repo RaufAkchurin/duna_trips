bc2f579005219def96e3751436daaafa94485b4f
RaufAkchurin <abdurauf.dev@gmail.com>
2024-01-01 10:36:45 +0600

    DB_TEST added

--- trip_admin/trip_admin/settings.py
+++ trip_admin/trip_admin/settings.py
@@ -78,12 +78,22 @@ WSGI_APPLICATION = 'trip_admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+LOCALHOST_IP = os.getenv('LOCALHOST_IP')
+
+if LOCALHOST_IP in ['127.0.0.1:8000', '0.0.0.0:8000']:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db_test.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -131,3 +141,5 @@ MEDIA_ROOT = '/root/duna_trips/trip_admin/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import socket
+print(f"GETHOST getted {socket.gethostname()}")
