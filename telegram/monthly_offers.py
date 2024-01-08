86e3f0da417a645c78f14ab37dd0c814ef420dac
RaufAkchurin <abdurauf.dev@gmail.com>
2024-01-08 11:31:34 +0600

    bug bix

--- telegram/monthly_offers.py
+++ telegram/monthly_offers.py
@@ -2,10 +2,11 @@ import os
 from datetime import datetime
 from aiogram.enums import ParseMode
 from dotenv import load_dotenv
-from aiogram import Bot, types
+from aiogram import Bot
 from API import get_post_list, get_grouped_prices_by_month
 from special_offers import package_of_destinations, data_formatted, link_generator_ticket, price, weekday, \
     get_photo_path_by_host_ip
+from telegram.pictures_generator import picture_generator
 
 load_dotenv()
 GROUP_CHAT_ID = os.getenv('GROUP_CHAT_ID')
@@ -14,8 +15,12 @@ GROUP_CHAT_ID = os.getenv('GROUP_CHAT_ID')
 def sorting_tickets_by_price(tickets, count_of_tickets_in_direction):
     # Здесь мы выдёргиваем все билеты на месяц, сортируем по цене и выдёргиваем 5 самых дешевых
 
-    # Extract flight records from the data dictionary
-    flights = list(tickets.values())
+    # TODO тут часто падает, добавить исключения
+    if tickets is not None:
+        # Extract flight records from the data dictionary
+        flights = list(tickets.values())
+    else:
+        return None
 
     # Sort flights by price
     sorted_flights = sorted(flights, key=lambda x: x['price'])
@@ -80,6 +85,16 @@ def monthly_offers_message(post):
     return message
 
 
+def get_destination_name(post):
+    destinations = post['destinations']
+    index = post['last_viewed_destination_index'] + 1
+
+    if index >= len(destinations):
+        index = index % len(destinations)
+
+    return destinations[index]['destination_name']
+
+
 async def send_monthly_offers(bot: Bot):
     posts = get_post_list()
     try:
@@ -89,9 +104,10 @@ async def send_monthly_offers(bot: Bot):
             if file_name:
                 picture_path = await get_photo_path_by_host_ip(file_name)
             message = monthly_offers_message(post)
+            path = picture_generator(get_destination_name(post=post))
             if message:
                 await bot.send_photo(chat_id=chat_id,
-                                     photo=types.FSInputFile(path=picture_path))
+                                     photo=path)
 
                 await bot.send_message(chat_id=chat_id,
                                        text=message,
@@ -100,7 +116,7 @@ async def send_monthly_offers(bot: Bot):
 
     except Exception as e:
         await bot.send_message(chat_id='5640395403',
-                               text="ERROR TEXT for monthly_offers - " + str(e),
+                               text="ERROR TEXT for monthly_offers " + str(e),
                                parse_mode=ParseMode.MARKDOWN,
                                disable_web_page_preview=True,
                                protect_content=False)
---
+++ telegram/pictures_generator.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+def picture_generator(prompt: str):
+    load_dotenv()
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+    client = OpenAI(api_key=OPENAI_API_KEY)
+
+    response = client.images.generate(
+        model="dall-e-2",
+        prompt=prompt,
+        size="1024x1024",
+        quality="hd",
+        style="vivid",
+        n=1,
+    )
+
+    image_url = response.data[0].url
+
+    return image_url
